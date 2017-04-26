1236c08e2b3efb16c6e0524794d2eff95f3dac6f
wolfram12345 <wolfram657890587@yandex.ru>
2017-04-26 19:01:47 +0300

    Tests

--- src/test/scala/lectures/functions/AuthenticationTest.scala
+++ src/test/scala/lectures/functions/AuthenticationTest.scala
@@ -1,7 +1,8 @@
 package lectures.functions
 
 import org.scalacheck.Gen
-import org.scalatest.WordSpec
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.prop.PropertyChecks
 
 import scala.util.Random
 
@@ -18,26 +19,51 @@ import scala.util.Random
   * * * * * 1 тест на их композицию
   *
   */
-class AuthenticationTest extends WordSpec {
+class AuthenticationTest extends WordSpec with PropertyChecks with Matchers{
 
-  val cardUserCreds = CardCredentials(12345678)
-  val cardUserCreds2 = CardCredentials(87654321)
-  val authUserCreds = LPCredentials("qwerty", "qwerty")
-  val authUserCreds2 = LPCredentials("qwerty2", "qwerty2")
+  val cardUserGenerator: Gen[CardUser] = Gen.const(CardUser())
+  val lpUserGenerator: Gen[LPUser] = Gen.const(LPUser())
+  val lpAndCardUsersGenerator: Gen[User] = Gen.const({
+    if(Random.nextBoolean())
+      LPUser()
+    else
+      CardUser()
+  })
 
-  "authByCardTest" when{
-    "noError" should {
-      val p = Gen.delay()
-//      val testUsersGen: Gen[User] = Gen.map(w => {
-//        val randomValue = Random.nextInt(3)
-//        if(randomValue == 0)
-//          AnonymousUser()
-//        else if(randomValue == 1)
-//          LPUser()
-//        else
-//          CardUser()
-//      }
+
+  implicit override val generatorDrivenConfig = PropertyCheckConfig(minSize = 10, maxSize = 40)
+
+  "authByCardTest" should{
+    "firstTestAuthByCard" in{
+      val ms = generatorDrivenConfig.maxSize
+      val cardUsersWithoutCardUserCreads = (1 to ms) flatMap(_ => cardUserGenerator.sample)
+      cardUsersWithoutCardUserCreads.filter(cardUser => Authentication.authByCard.lift(cardUser).isDefined) should have size 0
+    }
+    "secondTestAuthByCard" in{
+      val ms = generatorDrivenConfig.minSuccessful
+      val cardUsersWithCardUserCreads = ((1 to ms) flatMap(_ => cardUserGenerator.sample)) :+ CardUser(1234, AuthenticationData.cardUserCreds)
+      cardUsersWithCardUserCreads.filter(cardUser => Authentication.authByCard.lift(cardUser).isDefined) should have size 1
     }
 
+  }
+
+  "authByLPtest" should{
+    "firstTestAuthByLP" in{
+      val ms = generatorDrivenConfig.maxDiscarded
+      val lpUsersWithoutLPCredentials = (1 to ms) flatMap(_ => lpUserGenerator.sample)
+      lpUsersWithoutLPCredentials.filter(lpUser => Authentication.authByLP.lift(lpUser).isDefined) should have size 0
+    }
+    "secondTestAuthByLP" in{
+      val ms = generatorDrivenConfig.workers
+      val lpUsersWithLPCredentials = ((1 to ms) flatMap(_ => lpUserGenerator.sample)) :+ LPUser(5678, AuthenticationData.authUserCreds)
+      lpUsersWithLPCredentials.filter(lpUser => Authentication.authByLP.lift(lpUser).isDefined) should have size 0
+    }
+  }
+
+  "composeTest" in{
+    val countUsers = Random.nextInt(100) + 10
+    val users = (1 to countUsers) flatMap(_ => lpAndCardUsersGenerator.sample)
+    users.filter(user => Authentication.authByLP.orElse(Authentication.authByCard).lift(user).isDefined) should have size 0
+    (users :+ CardUser(1234, AuthenticationData.cardUserCreds)).filter(user => Authentication.authByLP.orElse(Authentication.authByCard).lift(user).isDefined) should have size 1
   }
 }
